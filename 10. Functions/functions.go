86e3bb27dbc3bc9f32a97d63379adfece914e928
Thiago <thiagomowszet@gmail.com>
2023-01-12 17:17:50 -0300

    functions and function with parameters/arguments

---
+++ 10. Functions/functions.go
@@ -0,0 +1,39 @@
+package main
+
+import("fmt")
+
+
+func functions_go(){
+
+// Syntax: function_name(){}
+
+// A function can be execute multiple times
+
+
+    fmt.Println("Hello, this came from a function!!")
+
+
+/*
+
+Naming rules
+
+A function name must start with a letter
+A function name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Function names are case-sensitive
+A function name cannot contain spaces
+If the function name consists of multiple words, techniques introduced for multi-word variable naming can be used
+
+*/
+
+
+
+
+}
+
+
+func main(){
+
+    functions_go()
+    functions_go()
+    functions_go()
+}
---
+++ 10. Functions/paramerts_arguments_functions.go
@@ -0,0 +1,27 @@
+package main
+
+import ("fmt")
+
+func examples(name string) {
+
+// Function with 1 parameter
+
+	fmt.Println("Hello, my name is ", name)
+
+}
+
+
+func examples_2(name string, age int, coutry string){
+
+    fmt.Println("Hello", age, "years old" , name, "from", coutry)
+}
+
+func main() {
+
+// Syntax: FunctionName(param1 type, param2 type, param3 type) {code to be executed}
+
+// "Thiago" is our argument: 
+
+	examples("Thiago")
+    examples_2("Thiago", 21, "Argentina")
+}
