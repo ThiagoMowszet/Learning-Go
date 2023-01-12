4c3e1930075d6deff76aab56bce3f16a02895308
Thiago <thiagomowszet@gmail.com>
2023-01-12 12:27:02 -0300

    multi-cases switch

---
+++ 8. Switch/multi-case_switch.go
@@ -0,0 +1,25 @@
+package main 
+
+import("fmt")
+
+
+func multi_cases(){
+    day := 5
+
+    switch day {
+    case 1,3,5:
+        fmt.Println("Odd weekday")
+    case 2,4:
+        fmt.Println("Even weekday")
+    case 6,7:
+        fmt.Println("Weekend")
+    default:
+        fmt.Println("Invalid day of day number")
+    }
+
+}
+
+func main(){
+
+    multi_cases()
+}
